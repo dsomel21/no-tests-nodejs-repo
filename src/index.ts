d207cf32bea60ff19a065179d943bb3ab38e4dc2
Dilraj <dsomel21@gmail.com>
2021-02-13 15:11:39 -0500

    SETUP - Static script for checking-types and 'lint' have been added

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-  const coolCar: Car = {
+const coolCar: Car = {
   make: 'Ferrari',
   // mode1: 'Testarossa', // Make sure this breaks
   model: 'Testarossa',
@@ -28,7 +28,7 @@ function add(a: number, b: number): number {
 // typeof coolCar === 'obect'; // Make sure this "breaks"
 typeof coolCar === 'object';
 
-    function getFormattedCarDetails(car: Car): string {
+function getFormattedCarDetails(car: Car): string {
   const { make, model, engine } = car;
   const { cylinders, horsepower } = engine;
 
